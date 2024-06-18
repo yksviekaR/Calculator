755efacc20a31cd6c9c5c09fac0520ab8a9c118b
yksviekaR <168476908+yksviekaR@users.noreply.github.com>
2024-06-18 16:26:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   const [first, setFirst] = useState("");
   const [operation, setOperation] = useState(0)
+  const [sign, setSign] = useState("");
   const [secound, setSecound] = useState("");
   const [equal, setEqual] = useState("");
   const [isSec, setIsSec] = useState(false);
@@ -11,21 +12,26 @@ function App() {
   function handleEqual() {
     if(operation == 0){
       setEqual("Chose Operation");
+      setSign("(no operation selected)");
     }
     if(operation == 1){
-      setEqual(Number(first) + Number(secound));
+      setEqual(`= ${Number(first) + Number(secound)}`);
     }
     if(operation == 2){
-      setEqual(Number(first) - Number(secound));
+      setEqual(`= ${Number(first) - Number(secound)}`);
     }
     if(operation == 3){
-      setEqual(Number(first) * Number(secound));
+      setEqual(`= ${Number(first) * Number(secound)}`);
     }
     if(operation == 4){
-      setEqual(Number(first) / Number(secound));
+      if(Number(secound) == 0){
+        setEqual("Can't devide by 0")
+      }else{
+        setEqual(`= ${Number(first) / Number(secound)}`);
+      }
     }
     if(operation == 5){
-      setEqual(`${Math.pow(Number(first), 2)}  ${Math.pow(Number(secound), 2)}`);
+      setEqual(`= ${Math.pow(Number(first), 2)}  ${Math.pow(Number(secound), 2)}`);
     }
   }
 
@@ -34,18 +40,22 @@ function App() {
     <>
       <div className='container'>
         <div className='calcu'>
-          <input type="number" value={first} onChange={(e) => {setFirst(e.target.value)}} /> <br />
+          <p className='header'>React Calculator</p>
+          {/*<input type="number" value={first} onChange={(e) => {setFirst(e.target.value)}} /> <br />
           <input type="number" value={secound} onChange={(e) => {setSecound(e.target.value)}} /> <br />
-          <input type="text" value={equal} disabled={true} />
-          <div className='inputs'>
-            <button onClick={() => {setFirst(""); setSecound(""); setEqual(""); setIsSec(false)}}>C</button>{/*CLEAR*/}
-            <button onClick={() => {setOperation(1); setIsSec(true)}}>+</button>{/*ADDITION*/}
-            <button onClick={() => {setOperation(2); setIsSec(true)}}>-</button>{/*SUBTRACTION*/}
+          <input type="text" value={equal} disabled={true} />*/}
+          <div className='display'>
+            <p>{first} {sign} {secound} {equal}</p>
           </div>
           <div className='inputs'>
-            <button onClick={() => {setOperation(3); setIsSec(true)}}>x</button>{/*MULTIPLICATION*/}
-            <button onClick={() => {setOperation(4); setIsSec(true)}}>÷</button>{/*DEVIDE*/}
-            <button onClick={() => {setOperation(5); setIsSec(true)}}>x²</button>{/*SQUARE*/}
+            <button onClick={() => {setFirst(""); setSecound(""); setEqual(""); setIsSec(false); setSign("")}}>C</button>{/*CLEAR*/}
+            <button onClick={() => {setOperation(1); setIsSec(true); setSign("+")}}>+</button>{/*ADDITION*/}
+            <button onClick={() => {setOperation(2); setIsSec(true); setSign("-")}}>-</button>{/*SUBTRACTION*/}
+          </div>
+          <div className='inputs'>
+            <button onClick={() => {setOperation(3); setIsSec(true); setSign("x")}}>x</button>{/*MULTIPLICATION*/}
+            <button onClick={() => {setOperation(4); setIsSec(true); setSign("/")}}>÷</button>{/*DEVIDE*/}
+            <button onClick={() => {setOperation(5); setIsSec(true); setSign("^")}}>x²</button>{/*SQUARE*/}
           </div>
           <div className='inputs'>
             <button onClick={() => {if(isSec == false){setFirst(first + 7)}else{setSecound(secound + 7)}}}>7</button>
@@ -63,9 +73,9 @@ function App() {
             <button onClick={() => {if(isSec == false){setFirst(first + 1)}else{setSecound(secound + 1)}}}>1</button>
           </div>
           <div className='inputs'>
-            <button>.</button>
+            <button onClick={() => {if(isSec == false){setFirst(first + ".")}else{setSecound(secound + "")}}}>.</button>
             <button onClick={() => {if(isSec == false){setFirst(first + 0)}else{setSecound(secound + 0)}}}>0</button>
-            <button onClick={handleEqual}>=</button>{/*EQUAL*/}
+            <button onClick={handleEqual} className='equal' >=</button>{/*EQUAL*/}
           </div>
         </div>
       </div>
